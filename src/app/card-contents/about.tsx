42328548d5806a71299e1939153579e75ce35bba
Joseph Deng <75818199+JosephDeng317@users.noreply.github.com>
2025-06-24 17:31:30 -0400

    added photos

--- src/app/card-contents/about.tsx
+++ src/app/card-contents/about.tsx
@@ -4,15 +4,34 @@ export default function About() {
   return (
     <div className="text-gray-200 opacity-100 mt-5 mx-5">
       <h1 className='text-6xl'>hello...</h1>
-      <div className='mt-5 mb-5 lg:w-1/2 pr-10'>
-        <p className='opacity-90 text-lg antic'>
-          Nice to meet you! I&apos;m Joseph, a passionate fullstack developer with more than 6 years 
-          of programming experience in various languages. I began my journey learning the fundementals of Python and Java in my 
-          spare time, and have since branched out into the world of Web Development and Machine Learning. In my most recent internship,
-          I worked at Metrized Inc., a fast paced Machine Learning startup based in Vancouver.
-          Currently, I am pursuing a Bachelor&apos;s degree in Computer Science and Physics at the University Of British Columbia, 
-          where I am in my third year. When I&apos;m not coding, you may find me making music, bouldering, running, skiing, or immersing myself in the beautiful nature surrounding Vancouver!
-        </p>
+      <div className="flex flex-col lg:flex-row">
+        <div className='mt-5 mb-5 lg:w-1/2 pr-10'>
+          <p className='opacity-90 text-lg antic'>
+            Nice to meet you! I&apos;m Joseph, a passionate fullstack developer with more than 6 years 
+            of programming experience in various languages. I began my journey learning the fundementals of Python and Java in my 
+            spare time, and have since branched out into the world of Web Development and Machine Learning. In my most recent internship,
+            I worked at Metrized Inc., a fast paced Machine Learning startup based in Vancouver.
+            Currently, I am pursuing a Bachelor&apos;s degree in Computer Science and Physics at the University Of British Columbia, 
+            where I am in my third year. When I&apos;m not coding, you may find me making music, bouldering, running, skiing, or immersing myself in the beautiful nature surrounding Vancouver!
+          </p>
+        </div>
+        <div className="flex">
+          <img 
+            src="matcha.JPG"
+            alt="performative man"
+            width="150"
+          />
+          <img 
+            src="ski.JPG"
+            alt="skiing"
+            width="150"
+          />
+          <img 
+            src="mountains.JPG"
+            alt="mountains"
+            width="400"
+          />
+        </div>
       </div>
     </div>
   )
